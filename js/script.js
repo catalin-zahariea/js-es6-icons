4f90217f1483261081e354248bb108d5894d8dd5
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-04-16 14:13:28 +0200

    Base structure.

---
+++ js/script.js
@@ -0,0 +1,130 @@
+// --------------------------------
+// JS - ES6 - Icons
+// --------------------------------
+// 1) Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout
+// 2) Coloriamo le icone per tipo
+// 3) Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+// --------------------------------
+// 
+// --------------------------------
+// Variables
+// --------------------------------
+// 
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+const colors = [
+    '#ff3b41',
+    '#a3d3d5',
+    '#84b71c'
+];
+// 
+// --------------------------------
+// To Print
+{/*
+	<div class="icon-element">
+    <i class="fas fa-cat"></i>
+    <div class="icon-name">Cat</div>
+</div>
+*/}
+// --------------------------------
+// 
+const printIcons (() => {
+
+});
